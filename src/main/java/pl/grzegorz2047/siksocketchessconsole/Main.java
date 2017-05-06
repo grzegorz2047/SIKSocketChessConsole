a465ca6c8bd2571203b4ed580f4e6f6fa4f0058f
grzegorz2047 <grzegorz2047@gmail.com>
2017-05-07 00:26:48 +0200

    init.

---
+++ src/main/java/pl/grzegorz2047/siksocketchessconsole/Main.java
@@ -0,0 +1,15 @@
+package pl.grzegorz2047.siksocketchessconsole;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Plik stworzony przez grzegorz2047 07.05.2017.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+            
+    }
+}
